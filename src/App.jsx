e7fe3a7b71fcdbddfde13687e3d31a20d25f6378
MaramLA <maramalfaraj2000@gmail.com>
2023-12-06 14:20:31 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Welcome to react-aws-test</h1>
+      <h3>Creator: Maram Alfaraj</h3>
+    </div>
+  );
+}
+
+export default App;
